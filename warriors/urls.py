9a05090a7eed08ef4bfe17bb9a350506634cdf44
Morteza Hajiabadi <hajiabadi1377@email.kntu.ac.ir>
2020-12-30 00:01:52 +0330

    media_root and media_url add to settings and urls

--- warriors/urls.py
+++ warriors/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from warriors.views.poem import *
 from warriors.views.login import *
 from warriors.views.profile import *
@@ -44,5 +46,5 @@ urlpatterns = [
     path('api/poem/comments/<int:poem_id>', get_comment, name='get comments'),
     path('api/poem/addcomment/', add_comment, name='add comment'),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
